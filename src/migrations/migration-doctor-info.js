8939bbfdf8fd89be4ac5625359a3d6bd8f094960
Van Dao <dao28901@gmail.com>
2025-01-16 16:00:58 +0700

    #78 Thêm Giao Diện Lưu Thông Tin Bác Sĩ và Table Doctor Infor

---
+++ src/migrations/migration-doctor-info.js
@@ -0,0 +1,53 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Doctor_info", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      doctorId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      priceId: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      paymentId: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      provinceId: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      addressClinic: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      nameClinic: {
+        type: Sequelize.STRING,
+      },
+      note: {
+        type: Sequelize.STRING,
+      },
+      count: {
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Doctor_info");
+  },
+};
